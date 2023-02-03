c5d0931aeaac08ba1c8535fc04c70c4b2bdd3c18
Mesha <madsketchz@gmail.com>
2023-02-03 10:20:20 -0800

    Write test 2 to calcuate mercury's age

--- __tests__/ageSpace.test.js
+++ __tests__/ageSpace.test.js
@@ -7,4 +7,9 @@ describe('AgeSpace', () => {
     expect(ageSpace.age).toEqual(42);
   });
 
+  test("should calculate Mercury years passed", () => {
+  const ageSpace = new AgeSpace(42);
+  expect(ageSpace.mercury).toEqual(175);
+  });
+
 })
