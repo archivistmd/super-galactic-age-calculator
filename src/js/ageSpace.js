3cb080957592444194a83117654db284d7b03efd
Mesha <madsketchz@gmail.com>
2023-02-03 10:52:41 -0800

    Pass mars test

--- src/js/ageSpace.js
+++ src/js/ageSpace.js
@@ -11,6 +11,8 @@ export default class AgeSpace {
     return Math.round(this.age / .67);
   } else if (this.age !== 0 && this.planet === "earth") {
     return this.age;
+  } else if (this.age !== 0 && this.planet === "mars") {
+    return Math.round(this.age * 1.88);
   } else {
     return this.age;
   }
