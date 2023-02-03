a66b232d4d62e73be3450222431296ffb925b158
Mesha <madsketchz@gmail.com>
2023-02-03 10:50:12 -0800

    Passed test 4

--- src/js/ageSpace.js
+++ src/js/ageSpace.js
@@ -9,6 +9,8 @@ export default class AgeSpace {
     return (this.age / .24);
   } else if (this.age !== 0 && this.planet === "venus") {
     return Math.round(this.age / .67);
+  } else if (this.age !== 0 && this.planet === "earth") {
+    return this.age;
   } else {
     return this.age;
   }
