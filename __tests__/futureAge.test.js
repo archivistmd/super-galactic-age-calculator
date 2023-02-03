8c279f625292f727079c0046579273d4a284f281
Mesha <madsketchz@gmail.com>
2023-02-03 14:44:48 -0800

    Add venus time test

--- __tests__/futureAge.test.js
+++ __tests__/futureAge.test.js
@@ -1,8 +1,15 @@
 import FutureAge from "../src/js/futureAge";
 
 describe('FutureAge', () => {
+
 test("should calculate mercury time", () => {
   const futureAge = new FutureAge (42, 56);
   expect(futureAge.mercuryTime).toEqual(58);
 });
+
+test("should calculate venus time", () => {
+  const futureAge = new FutureAge (42, 56);
+  expect(futureAge.venustime).toEqual(23);
+});
+
 });
