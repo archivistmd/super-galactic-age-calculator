7ff0eaaaf9d4c2f7e3f9a8c7f7499c6962508bd9
Mesha <madsketchz@gmail.com>
2023-02-12 23:50:38 -0800

    Insured all test ran after update

--- __tests__/ageSpace.test.js
+++ __tests__/ageSpace.test.js
@@ -17,28 +17,28 @@ describe('AgeSpace', () => {
     expect(ageSpace.planetAge()).toEqual(42);
   });
 
-  // test("should calculate Mercury years passed", () => {
-  // const ageSpace = new AgeSpace(42, "mercury");
-  // expect(ageSpace.planetAge()).toEqual(175);
-  // });
+  test("should calculate Mercury years passed", () => {
+  const ageSpace = new AgeSpace(42, "mercury");
+  expect(ageSpace.planetAge()).toEqual(175);
+  });
 
-  // test("should calculate Venus years passed", () => {
-  //   const ageSpace = new AgeSpace (42, "venus");
-  //   expect(ageSpace.planetAge()).toEqual(63);
-  // });
+  test("should calculate Venus years passed", () => {
+    const ageSpace = new AgeSpace (42, "venus");
+    expect(ageSpace.planetAge()).toEqual(63);
+  });
 
-  // test("should calculate earth years passed", () => {
-  //   const ageSpace = new AgeSpace (42, "earth");
-  //   expect(ageSpace.planetAge()).toEqual(42);
-  // });
+  test("should calculate earth years passed", () => {
+    const ageSpace = new AgeSpace (42, "earth");
+    expect(ageSpace.planetAge()).toEqual(42);
+  });
 
-  // test("should calculate Mars years passed", () => {
-  //   const ageSpace = new AgeSpace (42, "mars");
-  //   expect(ageSpace.planetAge()).toEqual(79);
-  // });
+  test("should calculate Mars years passed", () => {
+    const ageSpace = new AgeSpace (42, "mars");
+    expect(ageSpace.planetAge()).toEqual(79);
+  });
 
-  // test("should calculate Jupiter years passed", () => {
-  //   const ageSpace = new AgeSpace (42, "jupiter");
-  //   expect(ageSpace.planetAge()).toEqual(498);
-  // });
+  test("should calculate Jupiter years passed", () => {
+    const ageSpace = new AgeSpace (42, "jupiter");
+    expect(ageSpace.planetAge()).toEqual(498);
+  });
 })
